ddc09dac80c08676cbbc4cccc1084a75ecad9133
Lee Scroggins <lscrggns@yahoo.com>
2021-07-16 09:01:13 -0600

    Implemented launcher activity to inspect database.

--- app/src/main/java/edu/cnm/deepdive/toptag/TopTagApplication.java
+++ app/src/main/java/edu/cnm/deepdive/toptag/TopTagApplication.java
@@ -2,17 +2,28 @@ package edu.cnm.deepdive.toptag;
 
 import android.app.Application;
 import com.facebook.stetho.Stetho;
+import edu.cnm.deepdive.toptag.service.TopTagDatabase;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Initializes (in the {@link #onCreate()} method) application-level resources. This class
  * <strong>must</strong> be referenced in {@code AndroidManifest.xml}, or it will not be loaded and
  * used by the Android system.
  */
-public class AppStarterApplication extends Application {
+public class TopTagApplication extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
+
+    TopTagDatabase.setContext(this);
+    TopTagDatabase
+        .getInstance()
+        .getApplicationChoiceDao()
+        .delete()
+        .subscribeOn(Schedulers.io())
+        .subscribe();
+
     Stetho.initializeWithDefaults(this);
   }
 
